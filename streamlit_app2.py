86dc8593ab97abb048faf400d9c6d9a65f5adf38
naseem-data-pilot <145326498+naseem-data-pilot@users.noreply.github.com>
2023-11-29 14:51:14 +0500

    added prediction explainer

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -1,11 +1,15 @@
+# Updated by: Naseem Amjad
+# Updated on: 29 Nov 2023
+
 import streamlit as st
 import pickle
 import numpy as np
 import plotly.express as px
 import os
+import shap
 
 os.environ["SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL"] = "False"
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def load_model():
     with open(r'rf_modelsss.pkl', 'rb') as file:
@@ -13,6 +17,9 @@ def load_model():
     return model[0]
  
 model = load_model()
+
+# Initialize SHAP Explainer
+explainer = shap.TreeExplainer(model)
  
 def predict_risk(age, sex, housing, saving_accounts, checking_account, purpose):
     # Define mappings for categorical variables
@@ -33,9 +40,10 @@ def predict_risk(age, sex, housing, saving_accounts, checking_account, purpose):
     input_array = np.array([[age, sex, housing, saving_accounts, checking_account, purpose]])
    
     # Make prediction
-    #prediction = model.predict(input_array)[0]  # For binary classification
     prediction = model.predict_proba(input_array)[0]  # For binary classification
-    return prediction
+
+    # Return both prediction and input_array
+    return prediction, input_array
  
 def show_predict_page():
     st.title('Credit Risk Prediction App')
@@ -56,14 +64,26 @@ def show_predict_page():
  
     ok = st.button('Predict Risk')
     if ok:
-        result = predict_risk(age, sex, housing, saving_accounts, checking_account, purpose)
-       
+        result, input_array = predict_risk(age, sex, housing, saving_accounts, checking_account, purpose)
+        
         # Display result in a pie chart using Plotly
         labels = ['Low Risk', 'High Risk']
-        values = [1 - result, result]  # Assuming result is the probability of high risk
+        values = [result[0], result[1]]  # Directly use the result values
         fig = px.pie(names=labels, values=values, title='Credit Risk Prediction')
         st.plotly_chart(fig)
         st.subheader(f"The predicted risk is: {result}")
- 
+
+        # Generate SHAP values for the input
+        shap_values = explainer.shap_values(input_array)
+
+        # Choose the SHAP values for the predicted class
+        predicted_class = np.argmax(result)
+        shap_values_for_input = shap_values[predicted_class]
+
+        # Display SHAP values using a force plot
+        st.subheader('Prediction Explanation')
+        shap.force_plot(explainer.expected_value[predicted_class], shap_values_for_input, feature_names=['Age', 'Sex', 'Housing', 'Saving accounts', 'Checking account', 'Purpose'], matplotlib=True)
+        st.pyplot()
+        
 # Usage
 show_predict_page()
